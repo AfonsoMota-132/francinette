f0b3e53121b9724f7a57cd523f38253e1439f5d9
AfonsoMota-132 <afonsomota88@gmail.com>
2024-10-29 21:11:53 +0000

    forgot this smh

--- utils/install_zshrc.sh
+++ utils/install_zshrc.sh
@@ -11,23 +11,23 @@ printf "\nfi" "$HOME" >> "$RC_FILE"
 
 printf "\nif ! docker image ls | grep "francinette-image" &> /dev/null; then" "$HOME" >> "$RC_FILE"
 printf "\n\t\techo \"[Francinette] Loading the docker container\"" "$HOME" >> "$RC_FILE"
-printf "\n\t\tdocker load < %s/francinette-image/francinette.tar" "$HOME" >> "$RC_FILE"
+printf "\n\t\tdocker load < %s/francinette/francinette-image/francinette.tar" "$HOME" >> "$RC_FILE"
 printf "\n\t\texec \"$SHELL\"" "$HOME" >> "$RC_FILE"
 printf "\nfi" "$HOME" >> "$RC_FILE"
 
 printf "\nif ! docker ps | grep \"francinette-image\" &> /dev/null; then" "$HOME" >> "$RC_FILE"
-printf "\n\tif docker run -d -i -v /home:/home -v /goinfre:/goinfre -v /sgoinfre:/sgoinfre -v %s/francinette-image/logs:/francinette/logs-t --name run-paco francinette-image /bin/bash 2>&1 | grep \"already\" &> /dev/null; then" "$HOME" >> "$RC_FILE"
+printf "\n\tif docker run -d -i -v /home:/home -v /goinfre:/goinfre -v /sgoinfre:/sgoinfre -v %s/francinette/francinette-image/logs:/francinette/logs-t --name run-paco francinette-image /bin/bash 2>&1 | grep \"already\" &> /dev/null; then" "$HOME" >> "$RC_FILE"
 printf "\n\t\tdocker start run-paco" "$HOME" >> "$RC_FILE"
 printf "\n\tfi" "$HOME" >> "$RC_FILE"
 printf "\nfi" "$HOME" >> "$RC_FILE"
 fi
 
 if ! grep "francinette=" "$RC_FILE" &> /dev/null; then
-	printf "\nalias francinette=%s/francinette-image/run.sh\n" "$HOME" >> "$RC_FILE"
+	printf "\nalias francinette=%s/francinette/francinette-image/run.sh\n" "$HOME" >> "$RC_FILE"
 fi
 
 if ! grep "paco=" "$RC_FILE" &> /dev/null; then
-	printf "\nalias paco=%s/francinette-image/run.sh\n" "$HOME" >> "$RC_FILE"
+	printf "\nalias paco=%s/francinette/francinette-image/run.sh\n" "$HOME" >> "$RC_FILE"
 fi
 
 WHITE='\033[0;37m' 
