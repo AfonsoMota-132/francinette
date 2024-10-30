29941a92b1f09983990807bf134e67a0528fb3d1
Afonso Goncalves Mota <afogonca@c1r15s5.42porto.com>
2024-10-30 09:58:26 +0000

    some updates

--- utils/move_tmp.sh
+++ utils/move_tmp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p $HOME/.tmp_francinette
-if ls -l $HOME/francinette-image | grep "francinette.tar" &> /dev/null; then
-    mv /home/$USER/francinette-image/francinette.tar /home/$USER/.tmp_francinette/
+mkdir -p $HOME/francinette/.tmp_francinette
+if ls -l $HOME/francinette/francinette-image | grep "francinette.tar" &> /dev/null; then
+    mv /home/$USER/francinette/francinette-image/francinette.tar /home/$USER/francinette/.tmp_francinette/
 fi
